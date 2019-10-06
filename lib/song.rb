f2f67ce69a10fcd7bc675159b5dcf57722302366
James Benjamin <james.abenj@gmail.com>
2019-10-06 23:35:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,29 @@
+require "pry"
 class Song
-  attr_accessor :name, :artist_name
+  attr_accessor :name 
   @@all = []
-
-  def self.all
+  
+  def self.create
+    song = self.new 
+    @@all << song 
+    song 
+  end
+  
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name 
+  end  
+  
+  def self.create_by_name(song_name)
+    song = self.new 
+    song.name = song_name 
+    @@all << song 
+  end 
+  
+   def self.all
     @@all
   end
-
-  def save
-    self.class.all << self
-  end
+  
+  
 
 end
