b90b75bc1b539111cc9f644aa77e45b717a753a6
James Benjamin <james.abenj@gmail.com>
2019-10-06 23:43:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,6 +22,14 @@ class Song
     song 
   end 
   
+  def self.find_by_name(song_name)
+    @@all.each do |song|
+      if song.name == song_name
+        song 
+      end
+    end 
+    
+  
    def self.all
     @@all
   end
