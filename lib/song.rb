17296bd9b295eb7b84a78ae5cc7bd43501169168
James Benjamin <james.abenj@gmail.com>
2019-10-06 23:40:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,12 +12,14 @@ class Song
   def self.new_by_name(song_name)
     song = self.new
     song.name = song_name 
+    song
   end  
   
   def self.create_by_name(song_name)
     song = self.new 
     song.name = song_name 
     @@all << song 
+    song 
   end 
   
    def self.all
