218cfe994e68ba73a4b3c7265002c13ad368d1d7
James Benjamin <james.abenj@gmail.com>
2019-10-07 00:04:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,7 @@ class Song
       end
       
   def self.find_or_create_by_name(song_name)
+    @@all.each do |song|
     if self.find_by_name(song_name)
       return song 
     else self.create_by_name(song_name)
